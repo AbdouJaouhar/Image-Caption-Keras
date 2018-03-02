75c60cb28d6bc3c2f3f8ba4551e0f2aafcebcfe7
AbdouJaouhar <34003281+AbdouJaouhar@users.noreply.github.com>
2018-03-02 16:46:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from im2txt import Im2Txt
 if __name__ == '__main__':
 	ImModel = Im2Txt()
 	#ImModel.Train()
-	ImModel.GenDescription(ImModel.extract_features('test.jpg'))
+	ImModel.GenDescription('test.jpg')
